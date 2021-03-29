f2bcf4feeda407c68587cb730197643020beff40
Tri Ho <18127236@student.hcmus.edu.vn>
2021-03-29 15:10:25 +0700

    [Load mess]
    done

--- app/src/main/java/data/repositories/MessageRepository.java
+++ app/src/main/java/data/repositories/MessageRepository.java
@@ -11,10 +11,14 @@ import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import data.models.Conversation;
@@ -59,7 +63,7 @@ public class MessageRepository {
         MutableLiveData<ArrayList<Message>> mess = new MutableLiveData<ArrayList<Message>>();
         ArrayList<Message> listMess = new ArrayList<Message>();
         Log.d("mess repo", "dang load convID: "+convID);
-        colRef.whereEqualTo("convID",convID).orderBy("time").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
+        colRef.whereEqualTo("convID",convID).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()){
@@ -68,6 +72,13 @@ public class MessageRepository {
                         listMess.add(doc.toObject(Message.class));
                     }
                     Log.d("mess repo", "Load xong mess 1");
+                    Log.d("mess repo", Integer.toString(listMess.size()));
+                    Collections.sort(listMess, new Comparator<Message>() {
+                        @Override
+                        public int compare(Message o1, Message o2) {
+                            return o1.getTime().compareTo(o2.getTime());
+                        }
+                    });
                     mess.postValue(listMess);
                 }else{
                     Log.d("mess repo", "fail roi");
